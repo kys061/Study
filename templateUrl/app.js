14ac6fe31f95e90a2243b1f962f19b2d4bc17767
yskang <kys061@gmail.com>
2015-10-12 19:47:47 +0900

    learn transclude and scope@ and templateUrl and ng-click and ng-show and ng-transclude

--- templateUrl/app.js
+++ templateUrl/app.js
@@ -1,33 +1,34 @@
-angular.module("phoneApp", ["ui.router"])
+angular.module("app", ["ui.router"])
 
 
         .config(function config($stateProvider){
             $stateProvider.state("index", {
               url: "",
-              controller: "AppCtrl as App",
+              //controller: "AppCtrl as App",
               templateUrl: "templates/first.html"
             })
         })
 
-        .controller('AppCtrl', ['$scope', function ($scope) {
-            this.sayHi = function(){
-              alert("hi")
-            }
-
-            return $scope.AppCtrl = this;
-        }])
-
-        .directive('phone', [function () {
+      //  .controller('AppCtrl', ['$scope', function ($scope) {
+      //  }])
+// 엘리먼트 디렉티브는 속성을 가질 수 있는데, 그 속성은 변수가 될 수 있다. 아래와 같이 title처럼,
+// 그리고 그 변수를 태그(zippy)의 속성으로 사용하면서, 그 속성에 값을 넣어 줄 수 있다.
+//
+        .directive("zippy", [function () {
           return {
-            restrict: 'A',
+            restrict: "E",
+            transclude: true,
             scope: {
-                dial:"&"  //
+              title:"@"
             },
-            template: '<input type="text" ng-model="value" />' +
-            '<div class="btn btn-default" ng-click="dial({message:value})">Call home</div>',
+            templateUrl: 'templates/zippy.html',
 
-            link: function (scope, iElement, iAttrs) {
-              console.log(scope);
+            link: function (scope) {
+              scope.isContentVisible = false;
+              scope.toggleContent = function () {
+                scope.isContentVisible = !scope.isContentVisible;
+                console.log(scope.isContentVisible);
+              }
             }
           };
         }])
