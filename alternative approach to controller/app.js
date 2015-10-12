a64d5c9d73a89515f101da1e8d7c8803b698281a
yskang <kys061@gmail.com>
2015-10-12 14:31:07 +0900

    Add alternative approach to controller

---
+++ alternative approach to controller/app.js
@@ -0,0 +1,33 @@
+angular.module("phoneApp", ["ui.router"])
+
+
+        .config(function config($stateProvider){
+            $stateProvider.state("index", {
+              url: "",
+              controller: "AppCtrl as App",
+              templateUrl: "templates/first.html"
+            })
+        })
+
+        .controller('AppCtrl', ['$scope', function ($scope) {
+            this.sayHi = function(){
+              alert("hi")
+            }
+
+            return $scope.AppCtrl = this;
+        }])
+
+        .directive('phone', [function () {
+          return {
+            restrict: 'A',
+            scope: {
+                dial:"&"  //
+            },
+            template: '<input type="text" ng-model="value" />' +
+            '<div class="btn btn-default" ng-click="dial({message:value})">Call home</div>',
+
+            link: function (scope, iElement, iAttrs) {
+              console.log(scope);
+            }
+          };
+        }])
